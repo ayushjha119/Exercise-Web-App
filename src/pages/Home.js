85413b355a1a9634f3c1a600d17f1a8e7ee54238
ayushjha119 <ayush.jha.it23@heritageit.edu.in>
2023-11-08 17:35:23 +0530

    Gym api website completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
-import { Box } from "@mui/material";
-import HeroBanner from "../components/HeroBanner";
-import SearchExercise from "../components/SearchExercise";
-import Exercises from "../components/Exercises";
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercise />
-      <Exercises />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
     </Box>
   );
 };
